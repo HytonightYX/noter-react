9323839400dfc835c7f9af5381b4cbe7dfca6819
HytonightYX <whosiyuan@qq.com>
2019-06-13 23:21:51 +0800

    feat: 改为生产环境

--- src/constants.js
+++ src/constants.js
@@ -4,13 +4,11 @@ export const GET_NOTE_BY_ID = 'GET_NOTE_BY_ID'
 export const ADD_NOTE = 'ADD_NOTE'
 export const CLEAN_USER_NOTES = 'CLEAN_USER_NOTES'
 
-
 export const LOG_OUT = 'LOG_OUT'
 export const LOG_IN = 'LOG_IN'
 export const UPDATE_USER = 'UPDATE_USER'
 
+export const API_URL = 'http://47.99.215.152:3035/api'
+// export const API_URL = 'http://localhost:3035/api'
 
-// export const API_URL = 'http://47.99.215.152:3035/api'
-export const API_URL = 'http://localhost:3035/api'
-
-export const GITHUB_CLIENT_ID = '3672b8255f18495d5093'
+export const GITHUB_CLIENT_ID = '907e089d8acd7d59d6ea'
